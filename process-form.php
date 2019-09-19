3f973c4b1597fbf6edccf37e0acbd4fbda9246f8
Simarpreet Singh <DoodlyDoodler@users.noreply.github.com>
2019-09-18 23:47:44 -0400

    Add files via upload

---
+++ process-form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>YOU'RE DONE !!!</title>
+		<link rel="stylesheet" href="css/stylesF.css" />
+	</head>
+	<body>
+		<h1>YOUR DELIVERY IS ON THE WAY</h1>
+<?php
+	$fname = $_GET['fname'];
+	$lname = $_GET['lname'];
+		</body>
+</html>
